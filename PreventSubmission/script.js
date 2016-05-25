c38193c8a2e3a4e0a396b55d51e9e6abd7a15ad5
aminhp93 <minhpn.org.ec@gmail.com>
2016-05-25 01:52:52 -0700

    Building smart form

---
+++ PreventSubmission/script.js
@@ -0,0 +1,38 @@
+
+// // handle the form submit event
+// function prepareEventHandlers() {
+// 	document.getElementById("frmContact").onsubmit = function() {
+// 		// prevent a form from submitting if no email.
+// 		if (document.getElementById("email").value == "") {
+// 			document.getElementById("errorMessage").innerHTML = "Please provide at least an email address!";
+// 			// to STOP the form from submitting
+// 			return false;
+// 		} else {
+// 			// reset and allow the form to submit
+// 			document.getElementById("errorMessage").innerHTML = "";
+// 			return true;
+// 		}
+// 	};
+// }
+
+// // when the document loads
+// window.onload =  function() {
+// 	prepareEventHandlers();
+// };
+
+function prepareEventHandlers() {
+	document.getElementById("frmContact").onsubmit = function() {
+		if (document.getElementById("email").value == "")
+		{
+			document.getElementById("errorMessage").innerHTML = "Please provide at least an email address!";
+			return false;
+		} else {
+			document.getElementById("errorMessage").innerHTML = "";
+			return true;
+		}
+	};
+}
+
+window.onload = function() {
+	prepareEventHandlers();
+};
